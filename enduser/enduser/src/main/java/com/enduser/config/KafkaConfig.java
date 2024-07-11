b665ef7d724f51e8a258155996ffe4bda0536205
Prasad Malwadkar <malwadkarprasad@gmail.com>
2024-07-11 11:31:59 +0530

    kafka service added

---
+++ enduser/enduser/src/main/java/com/enduser/config/KafkaConfig.java
@@ -0,0 +1,14 @@
+package com.enduser.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Configuration
+public class KafkaConfig {
+
+    @KafkaListener(topics=AppConstant.LOCATION_UPDATE_TOPIC,groupId = AppConstant.GROUP_ID)
+    public void updateLocation(String value){
+        System.out.println(value);
+    }
+}
